5ff1bcf42c1edf2efe323eb861970bb697941c14
Harikrishnan <harikrishnankannoth@gmail.com>
2023-01-05 17:07:05 +0530

    Added Random Function + Button Animation + Sequence Array

--- game.js
+++ game.js
@@ -1,1 +1,21 @@
-alert("hello");
+let buttonColors = ["red","blue","green","yellow"]
+let gamePattern = [];
+
+let nextSequence = ()=>{
+    let num = Math.random()*4;
+    console.log(num)
+    return num;
+}
+
+//buttonColors[parseInt(nextSequence())] gives buttonColors[int] where int is the random number generated
+
+gamePattern.push(buttonColors[parseInt(nextSequence())]);
+$("#" + buttonColors[parseInt(nextSequence())]).fadeIn(100).fadeOut(100).fadeIn(100);
+
+console.log(gamePattern);
+
+colorChosen= buttonColors[parseInt(nextSequence())]
+console.log("./sounds/"+buttonColors[parseInt(nextSequence())]+".mp3")
+let audio = new Audio("sounds/" + colorChosen + ".mp3");
+audio.play();
+
